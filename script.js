87dd67ed1287634f89916589dfe58c2fbdd7eb4a
Jeisson Forero <jeissonf-96@hotmail.com>
2022-10-06 19:45:03 -0500

    adelanto segunda preentrega

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const precioDiagnostico = 75000;
-const precioMantenimiento = 160000;
-const precioReparacion = 200000;
+function Servicio(tipoServicio, precio) {
+  this.tipoServicio = tipoServicio;
+  this.precio = precio;
+}
+/* 1. Diagnostico \n 2. Mantenimiento \n 3. Reparacion*/
+const diagnostico = new Servicio("diagnostico", 75000);
+const mantenimiento = new Servicio("mantenimiento", 160000);
+const reparacion = new Servicio("reparacion", 160000);
+let servicios = [diagnostico, mantenimiento, reparacion];
 
-let costoTotal;
-let tipoServicio;
+function Repuestos(id, nombre, precio, stock) {
+  this.id = id;
+  this.nombre = nombre;
+  this.precio = precio;
+  this.stock = stock;
+}
+
+/* 1. Sensor de imagen \n 2. Tarjeta principal \n 3. Obturador \n 4. Flash" */
+const sensorImagen = new Repuestos(10, "Sensor de Imagen", 500000, 12);
+const tPrincipal = new Repuestos(11, "Tarjeta principal", 450000, 5);
+const obturador = new Repuestos(12, "Obturador", 145000, 4);
+const flash = new Repuestos(13, "Flash", 132000, 34);
+let repuestos = [sensorImagen, tPrincipal, obturador, flash];
 
 const formatearMoneda = new Intl.NumberFormat("es-CO", {
   style: "currency",
@@ -11,12 +28,31 @@ const formatearMoneda = new Intl.NumberFormat("es-CO", {
   minimumFractionDigits: 0,
 });
 
+let repuestosConStock = repuestos.filter((repuesto) => repuesto.stock > 0);
+let listaRepuestos = repuestosConStock.map((repuesto) => repuesto.nombre);
+
+function consultarExistencias() {
+  this.listaRepuestos = listaRepuestos.join("\n");
+  return this.listaRepuestos;
+}
+
+let costoTotal;
+function calcularCosto() {
+  if (tipoServicio === "diagnostico") {
+    costoTotal = servicios[0].precio * 1.19;
+  } else if (tipoServicio === "mantenimiento") {
+    costoTotal = servicios[1].precio * 1.19;
+    return costoTotal;
+  }
+}
+let tipoServicio = ""
 function solicitarServicio() {
   tipoServicio = prompt(
     "Escriba el tipo de servicio que requiere (sin tildes): \n 1. Diagnostico \n 2. Mantenimiento \n 3. Reparacion \n 4. Salir"
   ).toLowerCase();
 }
 
+/** inicio */
 alert(
   "Bienvenido a MN FOTO - Centro de Servicio Autorizado Nikon \n Nuestros servicions \n 1. Diagnostico de equipos \n 2. Mantenimiento general \n 3. Reparación según falla "
 );
@@ -25,7 +61,8 @@ solicitarServicio();
 while (tipoServicio != "salir") {
   switch (tipoServicio) {
     case "diagnostico":
-      costoTotal = precioDiagnostico * 1.16;
+      calcularCosto();
+      console.log(costoTotal);
       alert(
         "El costo inicial es de: " +
           formatearMoneda.format(costoTotal) +
@@ -35,7 +72,7 @@ while (tipoServicio != "salir") {
       break;
 
     case "mantenimiento":
-      costoTotal = precioMantenimiento * 1.16;
+      calcularCosto();
       alert(
         "El costo es de: " +
           formatearMoneda.format(costoTotal) +
@@ -45,45 +82,54 @@ while (tipoServicio != "salir") {
       break;
 
     case "reparacion":
-      costoTotal = precioReparacion;
+      alert(
+        "Actualmente tenemos estos repuestos en stock: \n" +
+          consultarExistencias()
+      );
       let referenciaRepuesto = prompt(
         "Escriba el repuesto que requiere de cambio: \n 1. Sensor de imagen \n 2. Tarjeta principal \n 3. Obturador \n 4. Flash"
       ).toLowerCase();
+      let total;
       switch (referenciaRepuesto) {
         case "1":
-          costoTotal = costoTotal + 50000;
+          total = (reparacion.precio + sensorImagen.precio) * 1.19;
           alert(
-            "El valor de la reparación con cambio de sensor de imagen es de: " +
-              formatearMoneda.format(costoTotal)
+            `El valor de la reparación con cambio de ${
+              sensorImagen.nombre
+            } es de: ${formatearMoneda.format(total)}`
           );
           break;
         case "2":
-          costoTotal = costoTotal + 100000;
+          total = (reparacion.precio + tPrincipal.precio) * 1.19;
           alert(
-            "El valor de la reparación con cambio de tarjeta principal es de: " +
-              formatearMoneda.format(costoTotal)
+            `El valor de la reparación con cambio de ${
+              tPrincipal.nombre
+            } es de: ${formatearMoneda.format(total)}`
           );
           break;
         case "3":
-          costoTotal = costoTotal + 200000;
+          total = (reparacion.precio + obturador.precio) * 1.19;
           alert(
-            "El valor de la reparación con cambio de obturador es de: " +
-              formatearMoneda.format(costoTotal)
+            `El valor de la reparación con cambio de ${
+              obturador.nombre
+            } es de: ${formatearMoneda.format(total)}`
           );
           break;
         case "4":
-          costoTotal = costoTotal + 50000;
+          total = (reparacion.precio + flash.precio) * 1.19;
           alert(
-            "El valor de la reparación con cambio de flash es de: " +
-              formatearMoneda.format(costoTotal)
+            `El valor de la reparación con cambio de flash es de: ${formatearMoneda.format(
+              total
+            )}`
           );
           break;
         default:
-          costoTotal = precioDiagnostico * 1.16;
+          tipoServicio = "diagnostico";
+          calcularCosto();
           alert(
-            "El costo es de " +
-              formatearMoneda.format(costoTotal) +
-              "que es el valor del diagnostico inicial y se le informara el costo de la reparación o mantenimiento según el resultado del diagnostico"
+            `El costo es de ${formatearMoneda.format(
+              costoTotal
+            )} el cual es el valor del diagnostico inicial y se le informara el costo de la reparación o mantenimiento según el resultado del diagnostico`
           );
           break;
       }
